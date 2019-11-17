421a6e5b9b9e081514ef517312a326543789e8f4
Nyandams <lecha.damien@gmail.com>
2019-11-18 00:46:52 +0100

    slider

--- src/components/common/NavbarHeader.js
+++ src/components/common/NavbarHeader.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Navbar} from 'react-bootstrap'
+import CheeseburgerMenu from "cheeseburger-menu";
+import {Link} from "react-router-dom";
 
-const NavbarHeader = () => (
-        <Navbar bg="dark" fixed="top">
-            <Navbar.Brand>
-                <img
-                    src="/assets/images/ancestree-logo.png"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top logoHeader"
-                    alt="Ancestree logo"
-                />
-                {" Ancestree"}
-            </Navbar.Brand>
-        </Navbar>
-);
+class NavbarHeader extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            menuOpen: false,
+        }
+    }
+
+    openMenu() {
+        this.setState({menuOpen: true })
+    }
+
+    closeMenu() {
+        this.setState({menuOpen: false })
+    }
+
+    render() {
+        return (
+        <React.Fragment>
+            <Navbar bg="dark" fixed="top">
+                <Navbar.Brand>
+                    <img
+                        onClick={() => this.setState({menuOpen: !this.state.menuOpen})}
+                        src="/assets/images/ancestree-logo.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top logoHeader"
+                        alt="Ancestree logo"
+                    />
+                    {" Ancestree"}
+                </Navbar.Brand>
+            </Navbar>
+            <CheeseburgerMenu isOpen={this.state.menuOpen} closeCallback={this.closeMenu.bind(this)} backgroundColor="#6c757d">
+                <div className="my-menu-content">
+                    <ul>
+                        <li><Link to="/home" onClick={this.closeMenu.bind(this)}>Menu item 1</Link></li>
+                        <li><Link to="/home" onClick={this.closeMenu.bind(this)}>Menu item 2</Link></li>
+                    </ul>
+                </div>
+            </CheeseburgerMenu>
+        </React.Fragment>
+        )
+    }
+
+}
 
 export default NavbarHeader;
