47811f11b3e974c37c98e81e4b7c0e9bb28474d2
Tallardserre <32522425+Tallardserre@users.noreply.github.com>
2019-11-21 00:33:17 +0100

    Refresh the token with MyDash (#25)

--- src/services/AuthAPIService.js
+++ src/services/AuthAPIService.js
@@ -1,4 +1,6 @@
 import authFetch from "./authFetch";
+import {MYDASH_REFRESH_ENDPOINT, LOGIN_CALLBACK_MYDASH} from "../conf/config"
+import { stringify as stringifyQS } from 'querystring';
 
 class AuthAPIService {
     domain = process.env.REACT_APP_AUTH_API;
@@ -47,7 +49,52 @@ class AuthAPIService {
         } else {
             throw new Error(`code ${result.status} : Token probably not valid`)
         }
+    }
 
+    disconnect() {
+        localStorage.removeItem("Authorization");
+        localStorage.removeItem("refresh_token")
+    }
+
+    /**
+     *
+     */
+    async refreshToken() {
+        const resAccessToken = await authFetch(MYDASH_REFRESH_ENDPOINT.url,
+            {
+                method: 'POST',
+                headers: {'Content-Type':'application/x-www-form-urlencoded'},
+                body: stringifyQS({
+                    'refresh_token': localStorage.getItem("refresh_token"),
+                    'client_id': process.env.REACT_APP_CLIENT_ID_MYDASH,
+                })
+            })
+            .catch(err => {
+                this.disconnect();
+                throw err
+            });
+
+        const accessTokenJson = await resAccessToken.json();
+        const dataToken = await authFetch(LOGIN_CALLBACK_MYDASH.url,
+            {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    'access_token': accessTokenJson.access_token
+                })
+            }).catch(err =>{
+            this.disconnect();
+            throw err
+        });
+
+        if (dataToken.status !== 201){
+            this.disconnect();
+            throw new Error(`code ${dataToken.status}: invalid refresh_token`);
+        }
+
+        const tokenJson = await dataToken.json();
+        localStorage.setItem("Authorization", tokenJson.token);
+        return tokenJson.token
     }
 
 
