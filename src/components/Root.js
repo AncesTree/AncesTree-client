84aeafcdb146c4ac50c460e922228b1dfddad67d
Thibaut <Thibaut.allard-serre@etu.umontpellier.fr>
2019-11-21 22:27:04 +0100

    Added profile page + fixed invitations

--- src/components/Root.js
+++ src/components/Root.js
@@ -18,6 +18,7 @@ import Invitation from "../components/Invitation";
 import Login from "./Login";
 import { Alert } from "react-bootstrap";
 import Register from "./Register";
+import Profile from "./profile/Profile";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -58,7 +59,7 @@ function Root() {
                 <Route exact path="/callback_linkedin_login" component={CallbackLoginLinkedIn}/>
                 <Route exact path="/callback_linkedin_join" component={CallbackRegisterLinkedIn}/>
                 <Route exact path="/callback_mydash" component={CallbackMyDash}/>
-                
+                <Route exact path="/me" component={App(withAuth(Profile))}/>
                 <Route exact path="/join/:id" component={App(JoinContainer)}/>
                 <Route exact path="/home" component={App(withAuth(Error404))}/>
                 <Route exact path="/tree" component={App(withAuth(TreeContainer))}/>
